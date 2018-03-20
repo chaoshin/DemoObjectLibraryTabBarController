470ace39639a94a09ffa5825dd85884ffffb92c9
Chao Shin <chao.shin@gmail.com>
2018-03-21 01:29:24 +0800

    Initial Commit

---
+++ DemoObjectLibraryTabBarController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoObjectLibraryTabBarController
+//
+//  Created by Chao Shin on 21/03/2018.
+//  Copyright © 2018 Chao Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
